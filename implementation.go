829e2e43e74463fbca00176c8bb884e891668c91
MytsV <mytsvictoria@gmail.com>
2023-03-17 21:13:52 +0200

    Implement basic postfix evaluation
    This commit adds postfix evaluation in a basic and not clean way.
    The added function is error prone and must be refactored and fixed.

--- implementation.go
+++ implementation.go
@@ -1,9 +1,48 @@
 package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
 
 // TODO: document this function.
-// PrefixToPostfix converts
+// EvaluatePostfix...
 func EvaluatePostfix(input string) (string, error) {
+	values := strings.Split(input, " ")
+	stack := make([]string, 0)
+	for _, value := range values {
+		if value == "+" || value == "-" || value == "*" || value == "/" || value == "^" {
+			index := len(stack) - 1
+			a, _ := strconv.Atoi(stack[index])
+			stack = stack[:index]
+			index = len(stack) - 1
+			b, _ := strconv.Atoi(stack[index])
+			stack = stack[:index]
+			var result int
+			if value == "+" {
+				result = a + b
+			}
+			if value == "*" {
+				result = a * b
+			}
+			if value == "/" {
+				result = b / a
+			}
+			if value == "-" {
+				result = b - a
+			}
+			if value == "^" {
+				result = int(math.Pow(float64(b), float64(a)))
+			}
+			stack = append(stack, strconv.Itoa(result))
+		} else {
+			stack = append(stack, value)
+		}
+	}
+	for _, value := range stack {
+		return value, nil
+	}
 	return "TODO", fmt.Errorf("TODO")
 }
