20536bf7793a6a5eadfe9dc355e1befda095ff13
Mila <gorbunova.milasha@gmail.com>
2023-03-19 19:56:11 +0200

    implemented ComputeHandler sruct and its Compute method

--- handler.go
+++ handler.go
@@ -1,12 +1,30 @@
 package lab2
 
-// ComputeHandler should be constructed with input io.Reader and output io.Writer.
-// Its Compute() method should read the expression from input and write the computed result to the output.
+import (
+	"io"
+)
+
+// The ComputeHandler structbe constructed with input io.Reader and output io.Writer.
 type ComputeHandler struct {
-	// TODO: Add necessary fields.
+	Input  io.Reader
+	Output io.Writer
 }
 
+// Its Compute() method should read the expression from input and write the computed result to the output.
 func (ch *ComputeHandler) Compute() error {
-	// TODO: Implement.
+	inputBuffer := make([]byte, 256)
+	n, err := ch.Input.Read(inputBuffer)
+	if err != nil {
+		return err
+	}
+	result, err := EvaluatePostfix(string(inputBuffer[:n]))
+	if err != nil {
+		return err
+	}
+	_, err = ch.Output.Write([]byte(result + "\n"))
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
