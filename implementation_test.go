77176c185b7e064bce61c167f1a26c3407b83c20
svitbar <svitbar1102@gmail.com>
2023-03-19 23:38:26 +0200

    refactored implementation test function

--- implementation_test.go
+++ implementation_test.go
@@ -1,9 +1,11 @@
 package lab2
 
 import (
+	"errors"
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestEvaluatePostfix(t *testing.T) {
@@ -12,7 +14,7 @@ func TestEvaluatePostfix(t *testing.T) {
 		name           string // test case name
 		input          string // function input
 		expectedResult string // expected outcome
-		hasError       bool   // a flag to check that an error is expected
+		expectedError  error  // a flag to check that an error is expected
 	}
 
 	// Define a slice of testCase as test table
@@ -22,113 +24,113 @@ func TestEvaluatePostfix(t *testing.T) {
 			name:           "Simple input; + operator",
 			input:          "4 0 4 + +",
 			expectedResult: "8",
-			hasError:       false,
+			expectedError:  nil,
 		},
 		{
 			name:           "Simple input; - operator",
 			input:          "104 506 -",
 			expectedResult: "-402",
-			hasError:       false,
+			expectedError:  nil,
 		},
 		{
 			name:           "Simple input; * operator",
 			input:          "3 4 *",
 			expectedResult: "12",
-			hasError:       false,
+			expectedError:  nil,
 		},
 		{
 			name:           "Simple input; / operator",
 			input:          "6 -3 /",
 			expectedResult: "-2",
-			hasError:       false,
+			expectedError:  nil,
 		},
 		{
 			name:           "Simple input; ^ operator",
 			input:          "11 3 ^",
 			expectedResult: "1331",
-			hasError:       false,
+			expectedError:  nil,
 		},
 		//Complex input
 		{
 			name:           "Complex input:0",
 			input:          "4 2 - 3 * 5 + -1 * -11 / 0 ^",
 			expectedResult: "1",
-			hasError:       false,
+			expectedError:  nil,
 		},
 		{
 			name:           "Complex input:1",
 			input:          "2 3 ^ 4 6 - + 6 * 3 / 2 ^",
 			expectedResult: "144",
-			hasError:       false,
+			expectedError:  nil,
 		},
 		//Invalid math operations
 		{
 			name:           "Division by zero",
 			input:          "1 0 /",
 			expectedResult: "",
-			hasError:       true,
+			expectedError:  errors.New("zero_division"),
 		},
 		{
 			name:           "Even root of the nagative number",
 			input:          "-1 0.5 ^",
 			expectedResult: "",
-			hasError:       true,
+			expectedError:  errors.New("imaginary_root"),
 		},
 		//Floating point numbers
 		{
 			name:           "Calculations with float numbers",
 			input:          "2.7 -0.3 - 0.1 *",
 			expectedResult: "0.3",
-			hasError:       false,
+			expectedError:  nil,
 		},
 		//Input ambiguities
 		{
 			name:           "Not enough operands in the stack",
 			input:          "4 0 4 + * -",
 			expectedResult: "",
-			hasError:       true,
+			expectedError:  errors.New("expression_incorrect"),
 		},
 		{
 			name:           "Not enough operators in the stack",
 			input:          "1 2 3 -",
 			expectedResult: "",
-			hasError:       true,
+			expectedError:  errors.New("expression_incorrect"),
 		},
 		{
 			name:           "No operands and operators",
 			input:          " ",
 			expectedResult: "",
-			hasError:       true,
+			expectedError:  errors.New("expression_incorrect"),
 		},
 		{
 			name:           "Has an extra space between operands",
 			input:          "2 6     2 - -",
 			expectedResult: "-2",
-			hasError:       false,
+			expectedError:  nil,
 		},
 		{
 			name:           "Has an extra space between operators",
 			input:          "2 6 2 -     -",
 			expectedResult: "-2",
-			hasError:       false,
+			expectedError:  nil,
 		},
 		{
 			name:           "A character that is not a number, not an operator:0",
 			input:          "4! 3 -",
 			expectedResult: "",
-			hasError:       true,
+			expectedError:  errors.New("invalid_operand"),
 		},
 		{
 			name:           "A character that is not a number, not an operator:1",
 			input:          "40e 39 %",
 			expectedResult: "",
-			hasError:       true,
+			expectedError:  errors.New("invalid_operand"),
 		},
 		{
 			name:           "A character that is not a number, not an operator:2",
 			input:          "Slava Ysu! Naviky slava!",
 			expectedResult: "",
-			hasError:       true,
+			expectedError:  errors.New("invalid_operand"),
 		},
 	}
 
@@ -136,11 +138,7 @@ func TestEvaluatePostfix(t *testing.T) {
 		actual, err := EvaluatePostfix(test.input)
 		assert.Equal(t, test.expectedResult, actual, test.name)
 
-		if test.hasError {
-			assert.NotNil(t, err, test.name)
-		} else {
-			assert.Nil(t, err, test.name)
-		}
+		assert.Equal(t, test.expectedError, err, test.name)
 	}
 }
 
