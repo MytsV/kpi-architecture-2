98ada701f42329daae50a7ae17b8923742f42e43
MytsV <mytsvictoria@gmail.com>
2023-03-19 17:20:40 +0200

    Add flag handling to main.go

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -2,26 +2,65 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"os"
+	"io"
+	"strings"
 	lab2 "github.com/MytsV/kpi-architecture-2"
 )
 
 var (
 	inputExpression = flag.String("e", "", "Expression to compute")
-	// TODO: Add other flags support for input and output configuration.
+	inputFile = flag.String("f", "", "Path to an input file")
+	outputFile = flag.String("o", "", "Path to an output file")
 )
+
+func getFile(path string) (file *os.File){
+	var err error
+	file, err = os.OpenFile(path, os.O_CREATE|os.O_RDWR, os.ModePerm)
+	if (err != nil) {
+		panic(err)
+	}
+	return
+}
 
 func main() {
 	flag.Parse()
+	if (*inputExpression != "" && *inputFile != "") {
+		panic("Multiple input flags specified")
+	}
 
-	// TODO: Change this to accept input from the command line arguments as described in the task and
-	//       output the results using the ComputeHandler instance.
-	//       handler := &lab2.ComputeHandler{
-	//           Input: {construct io.Reader according the command line parameters},
-	//           Output: {construct io.Writer according the command line parameters},
-	//       }
-	//       err := handler.Compute()
+	var (
+		reader io.Reader
+		writer io.Writer
+	)
 
-	res, _ := lab2.EvaluatePostfix("+ 2 2")
-	fmt.Println(res)
+	if (*inputExpression != "") {
+		reader = strings.NewReader(*inputExpression)
+	} else if (*inputFile != "") {
+		file := getFile(*inputFile)
+		defer file.Close()
+		reader = file
+	} else {
+		panic("No input flags specified")
+	}
+
+	if (*outputFile != "") {
+		file := getFile(*outputFile)
+		defer file.Close()
+		writer = file
+	} else {
+		writer = os.Stdout
+	}
+
+	buf := make([]byte, 1024)
+	//TODO: handle errors on working with io
+	n, _ := reader.Read(buf)
+	result, _ := lab2.EvaluatePostfix(string(buf[:n]))
+	writer.Write([]byte(result))
+
+	// handler := &lab2.ComputeHandler{
+	// 	Input: reader,
+	// 	Output: writer,
+	// }
+	//handler.Compute()
 }
