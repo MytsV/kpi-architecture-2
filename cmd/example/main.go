f3f0f6606faa99021e5d3bbe0c0c02d5a7e3545f
svitbar <svitbar1102@gmail.com>
2023-03-19 20:08:31 +0200

    implemented error handling

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -2,22 +2,24 @@ package main
 
 import (
 	"flag"
-	"os"
+	"fmt"
 	"io"
+	"os"
 	"strings"
+
 	lab2 "github.com/MytsV/kpi-architecture-2"
 )
 
 var (
 	inputExpression = flag.String("e", "", "Expression to compute")
-	inputFile = flag.String("f", "", "Path to an input file")
-	outputFile = flag.String("o", "", "Path to an output file")
+	inputFile       = flag.String("f", "", "Path to an input file")
+	outputFile      = flag.String("o", "", "Path to an output file")
 )
 
-func getFile(path string) (file *os.File){
+func getFile(path string) (file *os.File) {
 	var err error
 	file, err = os.OpenFile(path, os.O_CREATE|os.O_RDWR, os.ModePerm)
-	if (err != nil) {
+	if err != nil {
 		panic(err)
 	}
 	return
@@ -25,7 +27,7 @@ func getFile(path string) (file *os.File){
 
 func main() {
 	flag.Parse()
-	if (*inputExpression != "" && *inputFile != "") {
+	if *inputExpression != "" && *inputFile != "" {
 		panic("Multiple input flags specified")
 	}
 
@@ -34,9 +36,9 @@ func main() {
 		writer io.Writer
 	)
 
-	if (*inputExpression != "") {
+	if *inputExpression != "" {
 		reader = strings.NewReader(*inputExpression)
-	} else if (*inputFile != "") {
+	} else if *inputFile != "" {
 		file := getFile(*inputFile)
 		defer file.Close()
 		reader = file
@@ -44,7 +46,7 @@ func main() {
 		panic("No input flags specified")
 	}
 
-	if (*outputFile != "") {
+	if *outputFile != "" {
 		file := getFile(*outputFile)
 		defer file.Close()
 		writer = file
@@ -53,10 +55,24 @@ func main() {
 	}
 
 	buf := make([]byte, 1024)
-	//TODO: handle errors on working with io
-	n, _ := reader.Read(buf)
-	result, _ := lab2.EvaluatePostfix(string(buf[:n]))
-	writer.Write([]byte(result))
+
+	n, err := reader.Read(buf)
+	if err != nil && err != io.EOF {
+		fmt.Fprintf(os.Stderr, "Error reading input: %v\n", err)
+		os.Exit(1)
+	}
+
+	result, err := lab2.EvaluatePostfix(string(buf[:n]))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error evaluating postfix: %v\n", err)
+		os.Exit(1)
+	}
+
+	_, err = writer.Write([]byte(result))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing output: %v\n", err)
+		os.Exit(1)
+	}
 
 	// handler := &lab2.ComputeHandler{
 	// 	Input: reader,
